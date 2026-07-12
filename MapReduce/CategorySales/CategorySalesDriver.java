360d2ca4bfbec5d4cbdaa50f63378d0ad921e888
raniaedakkattil-design <rania.edakkattil@gmail.com>
2026-07-12 22:32:32 +0530

    Implement CategorySalesDriver for MapReduce job

---
+++ MapReduce/CategorySales/CategorySalesDriver.java
@@ -0,0 +1,31 @@
+package com.retail.analysis;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class CategorySalesDriver {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf, "Category Sales");
+
+        job.setJarByClass(CategorySalesDriver.class);
+
+        job.setMapperClass(CategorySalesMapper.class);
+        job.setReducerClass(CategorySalesReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
