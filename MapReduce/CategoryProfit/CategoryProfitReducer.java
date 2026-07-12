1bddbab47aa3f9c05f983162c79543eae7d09d9e
raniaedakkattil-design <rania.edakkattil@gmail.com>
2026-07-12 20:50:43 +0530

    Create CategoryProfitReducer.java

---
+++ MapReduce/CategoryProfit/CategoryProfitReducer.java
@@ -0,0 +1,22 @@
+package com.retail.analysis;
+
+import java.io.IOException;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class CategoryProfitReducer
+extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+
+    public void reduce(Text key,
+                       Iterable<DoubleWritable> values,
+                       Context context)
+            throws IOException, InterruptedException {
+
+        double total = 0;
+
+        for(DoubleWritable val : values)
+            total += val.get();
+
+        context.write(key, new DoubleWritable(total));
+    }
+}
